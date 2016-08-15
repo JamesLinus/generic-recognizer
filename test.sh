f8550bd2062e9431682bec24359ef0a807859bc8
lugon <lugon399@gmail.com>
2016-08-14 21:16:01 -0300

    Add .expect files.

--- test.sh
+++ test.sh
@@ -5,11 +5,12 @@ fail=0
 pass=0
 
 for gfile in `ls -v examples/*.ebnf` ; do
-    echo "==> Grammar: $gfile, String: string$strcnt"
-    ./genrec $gfile "examples/string$strcnt" -c >/dev/null
-    if [ "$?" = "0" ] ; then
+    ./genrec $gfile "examples/string$strcnt" -c >"examples/$strcnt.output" 2>/dev/null
+    if [ "$?" = "0" ] && cmp -s "examples/$strcnt.output" "examples/$strcnt.expect" ; then
+        echo "==> Grammar: $gfile, String: string$strcnt [PASS]"
         let pass=pass+1
     else
+        echo "==> Grammar: $gfile, String: string$strcnt [FAIL]"
         let fail=fail+1
     fi
     let strcnt=strcnt+1
